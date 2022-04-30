c6e492117dc5c5530619bfd3a97b01bc944a5ff9
Zoran Milutinovic <zoran.milutinovic.999@gmail.com>
2022-04-30 10:50:54 +1000

    Another attempt

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,7 +1,6 @@
-importScripts('/__/firebase/9.2.0/firebase-app-compat.js');
-importScripts('/__/firebase/9.2.0/firebase-messaging-compat.js');
-importScripts('/__/firebase/init.js');
-
+// importScripts('/__/firebase/9.2.0/firebase-app-compat.js');
+// importScripts('/__/firebase/9.2.0/firebase-messaging-compat.js');
+// importScripts('/__/firebase/init.js');
 
 
 
